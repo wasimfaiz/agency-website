12feb910f4e7f4355e4dda147ef03cffe0de91cc
Shaaz383 <asadakhtar800@gmail.com>
2026-02-13 17:23:42 +0530

    Making ready to deploy

--- .next/dev/server/pages/_app.js
+++
@@ -1,4 +0,0 @@
-var R=require("../chunks/ssr/[turbopack]_runtime.js")("server/pages/_app.js")
-R.c("server/chunks/ssr/[root-of-the-server]__5906c9eb._.js")
-R.m("[project]/Desktop/SetJets/agency/node_modules/next/app.js [ssr] (ecmascript)")
-module.exports=R.m("[project]/Desktop/SetJets/agency/node_modules/next/app.js [ssr] (ecmascript)").exports
--- .next/dev/static/chunks/Desktop_SetJets_agency_src_app_favicon_ico_mjs_8f9d36ba._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/9f354_next_dist_c28e61bc._.js"
-],
-    source: "dynamic"
-});
